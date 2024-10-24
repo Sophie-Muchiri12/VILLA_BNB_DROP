88f4c81916e2e9536f5eef358b4e5aca0c05968e
Sophie Muchiri <muthonisophie12@gmail.com>
2024-10-24 11:32:24 +0300

    changed app.py

--- servers/app.py
+++ servers/app.py
@@ -32,12 +32,12 @@ def create_app():
     from routes import create_routes
     create_routes(app,db)
 
-    # Add the after_request function for CORS headers
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        response.headers.add('Access-Control-Allow-Credentials', 'true')
-        return response
+    # # Add the after_request function for CORS headers
+    # @app.after_request
+    # def after_request(response):
+    #     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    #     response.headers.add('Access-Control-Allow-Credentials', 'true')
+    #     return response
     
 
     return app
